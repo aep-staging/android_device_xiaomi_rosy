6e256ec798f576f0b60576982882629a89f594ab
LuK1337 <priv.luk@gmail.com>
2020-05-16 20:28:31 +0200

    rosy: Build 64-bit variants of android.hardware.camera.{device,provider}
    * Fixes: ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.device@3.4.so" not found
    
    Change-Id: I938dd7135a53e9e0951c31e7f56082c0564cf924

--- device.mk
+++ device.mk
@@ -124,6 +124,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
