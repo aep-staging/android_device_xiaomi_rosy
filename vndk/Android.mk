d8b77f7683deb44ceddcbe3364dca26858d639db
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 02:59:23 +0100

    rosy: vndk: Copy vendor HIDL interface libs to /vendor
    Change-Id: Ia95e09b21ce42137cac9a1dd22f208fd9de9bf20

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -20,9 +20,9 @@ install_in_hw_dir := \
 
 vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
 
-define define-vndk-sp-lib
+define define-vndk-lib
 include $$(CLEAR_VARS)
-LOCAL_MODULE := $1.vndk-sp-gen
+LOCAL_MODULE := $1.$2
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1,,,,)/$1.so
 LOCAL_STRIP_MODULE := false
@@ -30,14 +30,15 @@ LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
+LOCAL_MODULE_RELATIVE_PATH := $3
+LOCAL_VENDOR_MODULE := $4
 LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
 
 ifneq ($$(TARGET_2ND_ARCH),)
 ifneq ($$(TARGET_TRANSLATE_2ND_ARCH),true)
 include $$(CLEAR_VARS)
-LOCAL_MODULE := $1.vndk-sp-gen
+LOCAL_MODULE := $1.$2
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1,,,$$(TARGET_2ND_ARCH_VAR_PREFIX),)/$1.so
 LOCAL_STRIP_MODULE := false
@@ -45,7 +46,8 @@ LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
+LOCAL_MODULE_RELATIVE_PATH := $3
+LOCAL_VENDOR_MODULE := $4
 LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
 endif # TARGET_TRANSLATE_2ND_ARCH is not true
@@ -61,7 +63,9 @@ $(foreach lib,$(VNDK_SAMEPROCESS_LIBRARIES),\
 VNDK_SP_LIBRARIES := $(filter-out libz,$(VNDK_SP_LIBRARIES))
 
 $(foreach lib,$(VNDK_SP_LIBRARIES),\
-    $(eval $(call define-vndk-sp-lib,$(lib))))
+    $(eval $(call define-vndk-lib,$(lib),vndk-sp-gen,$(vndk_sp_dir)$(if $(filter $(lib),$(install_in_hw_dir)),/hw),)))
+$(foreach lib,$(EXTRA_VENDOR_LIBRARIES),\
+    $(eval $(call define-vndk-lib,$(lib),vndk-ext-gen,,true)))
 
 install_in_hw_dir :=
 
@@ -69,6 +73,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := vndk-sp
 LOCAL_MODULE_OWNER := google
 LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
+LOCAL_REQUIRED_MODULES := \
+    $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES)) \
+    $(addsuffix .vndk-ext-gen,$(EXTRA_VENDOR_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
 endif
