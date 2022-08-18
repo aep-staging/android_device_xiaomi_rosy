cfc5a531c3b57fb3d02c2294fadd2331a4239508
Josh Wu <joshwu@google.com>
2022-08-18 12:36:22 +0530

    rosy: Switch Bt Audio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- device.mk
+++ device.mk
@@ -88,7 +88,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl:32
+    android.hardware.bluetooth.audio-impl:32
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
