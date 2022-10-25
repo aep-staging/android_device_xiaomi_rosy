1c820f2bee9dd80cea0dfc67ac70a6f409d4297c
Aditya Eka Pranata <aep.repository@gmail.com>
2022-10-25 16:56:44 +0700

    rosy: Remove FM Radio package
    Since FM Radio package not exists, remove for decrease size
    
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -189,11 +189,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.4.vendor
 
-# FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     libbacktrace-vendor \
