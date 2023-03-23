bbd95023d58f42f7ee8261b1ae299cd1d631e8d1
Aditya Eka Pranata <aep.repository@gmail.com>
2023-03-23 21:46:27 +0700

    rosy: Update to 4.9 kernel
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,14 +45,31 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.usbconfigfs=false androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 loop.max_part=7
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=false
+BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78af000
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/rosy
-TARGET_KERNEL_CONFIG := rosy-baunil_defconfig
-TARGET_KERNEL_VERSION := 3.18
+TARGET_KERNEL_CONFIG := rosy-perf_defconfig
+TARGET_KERNEL_VERSION := 4.9
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := atiga
+KERNEL_MAKE_FLAGS += CC=clang
+KERNEL_MAKE_FLAGS += CXX=clang++
+KERNEL_MAKE_FLAGS += CROSS_COMPILE=aarch64-linux-gnu-
+KERNEL_MAKE_FLAGS += CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+KERNEL_MAKE_FLAGS += AR=llvm-ar NM=llvm-nm
+KERNEL_MAKE_FLAGS += OBJDUMP=llvm-objdump
+KERNEL_MAKE_FLAGS += STRIP=llvm-strip
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
