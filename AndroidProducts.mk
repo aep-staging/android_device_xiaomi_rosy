saroj-nokia/device_xiaomi_rosy

rosy: Welcome Banana

AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_rosy.mk
+    $(LOCAL_DIR)/banana_rosy.mk
+
+COMMON_LUNCH_CHOICES := \
+    banana_rosy-user \
+    banana_rosy-userdebug \
+    banana_rosy-eng

aep-staging/android_device_xiaomi_rosy

AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_rosy.mk
+    $(LOCAL_DIR)/lineage_rosy.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_rosy-user \
+    lineage_rosy-userdebug \
+    lineage_rosy-eng